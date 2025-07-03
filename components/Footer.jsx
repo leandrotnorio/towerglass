d0c484761612f84c0e968f1df22740df4d3a2353
Seu Nome <seuemail@exemplo.com>
2025-07-03 10:43:54 -0300

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link'
+
+const logoagt = '/agtvidroslogo.svg'
+ 
+
 export default function Footer() {
   return (
-    <footer id="contato" className="bg-blue-800 py-4">
-      <div className="max-w-7xl mx-auto px-6 text-center">
-        <p className="mt-1">Contato: contato@engenhariavidros.com</p>
-        <p>&copy; {new Date().getFullYear()} Tower Glass. Todos os direitos reservados.</p>
+    <footer id="contato" className="bg-blue-800 mt-0 pt-2 pb-2">
+      <div className="max-w-7xl mx-auto px-2 py-0 text-center">
+        <Link  href="/">
+          <Image    
+            src="/agtvidroslogo.svg"
+            alt="Logo"
+            width={120}
+            height={30}
+            className="mx-auto block"/>
+        </Link>
+        <p className="mt-1 text-white">Contato: contato@engenhariavidros.com</p>
+        <p className="text-white">&copy; {new Date().getFullYear()} Tower Glass. Todos os direitos reservados.</p>
       </div>
     </footer>
   );
